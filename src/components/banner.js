d1d67d41f8843479964b0410667efbf5a7fc8188
user <user@HP>
2021-07-19 17:12:31 +0530

    banner commit

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from 'react'
 import { Link } from "react-router-dom";
-import logo from '../../assets/logo.png';
 import Query from "../Query";
 import GLOBAL_QUERY from "../../queries/global/global";
 
