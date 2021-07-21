04cbad7697c6735cd33fe77ce3cda676425e3eaf
user <user@HP>
2021-07-21 14:03:30 +0530

    banner commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -53,11 +53,11 @@ const Sliders = () => {
 
 
                                     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                                        <span class="glyphicon glyphicon-chevron-left"></span>
+                                        <span class="fa fa-chevron-circle-left"></span>
                                         <span class="sr-only">Previous</span>
                                     </a>
                                     <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                                        <span class="glyphicon glyphicon-chevron-right"></span>
+                                        <span class="fa fa-chevron-circle-right"></span>
                                         <span class="sr-only">Next</span>
                                     </a>
                                 </div></div></div></header>
