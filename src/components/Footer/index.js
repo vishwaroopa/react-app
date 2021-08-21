c128076184fd2d2986a1a966289423fed91024df
user <user@HP>
2021-08-21 10:45:14 +0530

    Add existing file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -115,7 +115,29 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div class="login-icon"><img src="https://res.cloudinary.com/mavin-infotech/image/upload/v1629469934/login_icon_aefbe2860f.png?7671780.100000024" /></div>
+                
+                <button type="button" class="btn btn-primary login-icon" data-toggle="modal" data-target="#exampleModal">
+                    <img src="https://res.cloudinary.com/mavin-infotech/image/upload/v1629469934/login_icon_aefbe2860f.png?7671780.100000024" />
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+      </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
                     )
