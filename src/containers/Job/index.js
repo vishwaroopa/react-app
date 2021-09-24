75e61b9301fb1a02ffd515c3c78e03ca3bc382f5
user <user@HP>
2021-09-24 18:26:59 +0530

    My first commit

--- src/containers/Job/index.js
+++ src/containers/Job/index.js
@@ -74,8 +74,7 @@ class Job extends React.Component {
         return post(url, formData, config)
     }
     render() {
-        const { id1 } = this.props.params;
-        console.log(id1);
+        console.log(this.props.params);
         let id = "full_stack_developer";
         return (
             <Query query={JOB_QUERY} slug={id}>
