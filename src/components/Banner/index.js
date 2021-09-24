4c0295c4a3af9cc5245515a890b46de47b80091e
user <user@HP>
2021-09-24 12:53:31 +0530

    My first commit

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Query from "../Query";
 import HOMEPAGE_QUERY from "../../queries/homepage/homepage";
+import ReactMarkdown from "react-markdown";
 
 const Banner = () => {
     return (
