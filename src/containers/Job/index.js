d3a73acdff80e9f381460a270a67ac292b1e52a8
user <user@HP>
2021-09-24 16:22:18 +0530

    My first commit

--- src/containers/Job/index.js
+++ src/containers/Job/index.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import Query from "../../components/Query";
 import ReactMarkdown from "react-markdown";
 import Moment from "react-moment";
+import axios, { post } from 'axios';
 
 import JOB_QUERY from "../../queries/jobs/job";
 import { Link } from "react-router-dom";
