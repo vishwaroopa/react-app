0a360033b84269edff07da40743a45b5c1e1de74
vishwaroopa <vishwarupa.vishu@gmail.com>
2021-11-19 11:30:12 +0530

    adding files

---
+++ src/components/Nav/index1.js
@@ -0,0 +1,5 @@
+/web/cgi-bin/php "$HOME/emplogin/mail-reminder.php"
+
+/web/cgi-bin/php5 "$HOME/emplogin/mail-reminder.php" > /dev/null 2>&1
+
+/web/cgi-bin/php "$HOME/html/emplogin/mail-reminder.php"
