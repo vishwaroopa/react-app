4613e5fd59f6d9b722b7e1c7c2a3e0ae09059bcd
vishwaroopa <vishwarupa.vishu@gmail.com>
2022-11-27 15:26:57 +0530

    Update index.js

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -7,7 +7,29 @@ import ReactMarkdown from "react-markdown";
 
 const Banner = () => {
     return (
-        
+        <Query query={HOMEPAGE_QUERY} id={null}>
+            {({ data: { homepage } }) => {
+                const imageUrl =
+                    process.env.NODE_ENV !== "development"
+                        ? homepage.banner1.image.url
+                        : process.env.REACT_APP_BACKEND_URL + homepage.banner1.image.url;
+                return (
+                    <section class="red-bg">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12">
+                                    <div class="features-14">
+                                        <h1 class="heading">{homepage.banner1.title}</h1>
+                                        <h1 class="heading"><ReactMarkdown source={homepage.contactbanner.text} /></h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+    )
+            }}
+
+        </Query>
     )
 }
 
